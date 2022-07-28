cc398ad4299f262e5ade962a0c1db7c9cd4e8928
emerson <emerson.cesario50@gmail.com>
2022-07-28 19:05:37 -0300

    add files

---
+++ README.md
@@ -0,0 +1,33 @@
+# Criar monitoramento Web zabbix via api 
+
+## Dependencies
+
+- Python3.8
+- zabbix_api
+
+## Requirements
+```
+pip3 install -r requirements.txt
+```
+## Pattern:
+
+# Como usar
+
+No arquivo urls.csv insira as urls que serão monitoradas conforme abaixo
+
+https://url.com
+https://url2.com
+https://url3.com
+
+# Execução do script
+```
+python3 create_web.py http://x.x.x.x/ Admin 
+```
+Será solicitado a senha 
+
+# Funcionamento do script
+
+O script irá verificar se existe um host e grupo de host chamado "Monitoramento URL" caso não irá criar estás variáveis podem ser mudadas no script, cada url recebe seu aplication e trigger a nível disaster em caso de falha maior que 3 
+
+## License
+GPLv3
---
+++ create_web.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+# Autor: Emerson Cesario
+# E-mail: emerson.cesario50@gmail.com
+
+from zabbix_api import ZabbixAPI,Already_Exists
+import csv
+import sys
+import getpass
+
+URL = sys.argv[1]
+USERNAME = sys.argv[2]
+PASSWORD = getpass.getpass("Digite a senha: ")
+
+try:
+    zapi = ZabbixAPI(URL, timeout=15)
+    zapi.login(USERNAME, PASSWORD)
+    print(f'Conectado na API do Zabbix, Versao Atual {zapi.api_version()}')
+    print ()
+except Exception as err:
+    print(f'Falha ao conectar na API do zabbix, erro: {err}')
+
+hostname = 'Monitoramento URL'
+group = 'Monitoramento URL'
+
+h = zapi.host.get({
+    "filter": {"host": [hostname]},
+})
+hg = zapi.hostgroup.get({
+    "filter": {'name': [group]}
+})
+
+if not h:
+ if not hg:
+  zapi.hostgroup.create({"name": group})   
+  hg = zapi.hostgroup.get({
+    "filter": {"host": [group]},
+  })[0]['groupid']
+  zapi.host.create({
+     "groups": [{ "groupid": hg}],
+     "host": hostname,
+     "proxy_hostid": "0",
+     "interfaces": {
+         "type": 1,
+         "main": 1,
+         "useip": 1,
+         "ip": "127.0.0.1",
+         "dns": "",
+         "port": "10050",
+         "details": {
+             "version": 2,
+             "bulk": 1,
+             "community": "{$SNMP_COMMUNITY}"
+         }
+     }
+  })   
+
+hostids = zapi.host.get({
+    "output": "extend",
+    "filter": {"host": [hostname]},
+    "selectHosts": ["hostid", "host"]
+    })[0]['hostid']
+
+def create_web(step):         
+        try:
+           nome = "Web Check " + step
+           try:
+            a = zapi.application.get({"output": 'extend',
+                                "hostid": hostids, 
+                                "filter":{'name': "Web Check " +step}
+                                })[0]['applicationid']
+           except Exception as IndexError: 
+            app = zapi.application.create({"name": "Web Check " +step,
+                                "hostid": hostids})
+
+            a = zapi.application.get({"output": 'extend',
+                                "hostid": hostids, 
+                                "filter":{'name': "Web Check " +step}
+                                })[0]['applicationid']
+
+           create_web = zapi.httptest.create({
+              "name": "Web Check "+ step,
+              "hostid": hostids,
+              "applicationid": a,
+              "steps": [
+               {
+                   "name": nome,
+                   "url": step,
+                   "status_codes": "200",
+                   "no": 1
+               }
+              ]
+           })
+           
+           trigger = zapi.trigger.create({"description": "Failed step of scenario URL: " + step,
+                                "expression": "{"+hostname+":web.test.fail["+nome+"].sum(#3)}>=3",
+                                "hostid": hostids,
+                                "priority": 5})
+           
+           print(f'URL(s) cadastrada {r}')
+        except Already_Exists:
+           print(f'URL(s) já cadastrada {r}')
+        except Exception as err:
+           print(f'Falha ao cadastrar a URL(s) {err}')
+
+with open('urls.csv') as file:
+    file_csv = csv.reader(file, delimiter=';')
+    for [r] in file_csv:
+        create_web(step=r)
+
+zapi.logout()
+   
